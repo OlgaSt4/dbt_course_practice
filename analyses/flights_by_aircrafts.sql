ca9c3b12b53955eabde2287d25af6d2fe20686c3
Olya <ol_st90@mail.ru>
2025-02-16 16:14:42 +0300

    Неделя 8, Jinja

--- analyses/flights_by_aircrafts.sql
+++ analyses/flights_by_aircrafts.sql
@@ -14,8 +14,9 @@ FROM
 
 SELECT
     {% for aircraft in important_aircrafts %}
-    SUM(CASE WHEN aircraft_id = '{{ aircraft }}' THEN 1 ELSE 0 END) as flights_{{aircraft}}
-        {%- if not loop.last %},{% endif %}
+    SUM(CASE WHEN aircraft_id = '{{ aircraft }}' THEN 1 ELSE 0 END) as flights_{{aircraft|title|replace('73','oo')}}
+        {%- if not loop['last'] %},{% endif %}
+        -- {{ loop.cycle('-', '+') }}
     {%- endfor %}
 FROM
     {{ ref('fct_flights') }}
