c65e649e3eeae4dc7b152a63c754430c12ef5ba9
Olya <ol_st90@mail.ru>
2025-04-27 21:32:10 +0300

    Неделя 16, Singular тесты

---
+++ tests/staging/flights/stg_flights__bookings__total_amount__value.sql
@@ -0,0 +1,12 @@
+{{
+    config(
+    severity = 'warn',
+    )
+}}
+SELECT
+    total_amount
+FROM
+    {{ ref('stg_flights__bookings') }}
+WHERE
+    total_amount > 10000000
+    OR total_amount <=0
